0739c4670633287830d57647ae470becac6977f5
Pablu23 <pablu23@gmx.de>
2024-09-21 15:11:11 +0200

    Add docker compose, initial working db to backend connection

--- backend/cmd/blinkscan/main.go
+++ backend/cmd/blinkscan/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/base64"
+	"log"
+
+	"crypto/rand"
+	"crypto/sha256"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/pablu23/blinkscan/backend/database"
+)
 
 func main() {
-  fmt.Println("Started")
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, "host=db user=postgres dbname=postgres password=secret sslmode=disable")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer conn.Close(ctx)
+
+	queries := database.New(conn)
+
+	accounts, err := queries.GetAccounts(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(accounts)
+
+	pwd := "test"
+	salt := make([]byte, 24)
+	rand.Read(salt)
+	hash := sha256.Sum256(append([]byte(pwd), salt...))
+	b64hash := base64.StdEncoding.EncodeToString(hash[:])
+	b64salt := base64.StdEncoding.EncodeToString(salt)
+
+	insertedAccount, err := queries.CreateAccount(ctx, database.CreateAccountParams{
+		Name:          "pablu",
+		Base64PwdHash: b64hash,
+		Base64PwdSalt: b64salt,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(insertedAccount)
 }
--- backend/schema.sql
+++ backend/schema.sql
@@ -1,16 +1,23 @@
 create table provider (
-  id uuid PRIMARY KEY not null,
+  id uuid PRIMARY KEY not null default gen_random_uuid(),
   url text not null,
   name text not null
 );
 
 create table image (
-  id uuid PRIMARY KEY not null,
+  id uuid PRIMARY KEY not null default gen_random_uuid(),
   path text not null
 );
 
+create table account (
+  id uuid PRIMARY KEY not null default gen_random_uuid(),
+  name text not null unique,
+  base64_pwd_hash text not null,
+  base64_pwd_salt text not null
+);
+
 create table manga (
-  id uuid PRIMARY KEY not null,
+  id uuid PRIMARY KEY not null default gen_random_uuid(),
   provider_id uuid not null,
   title text not null,
   thumbnail_id uuid,
@@ -24,7 +31,7 @@ create table manga (
 );
 
 create table chapter (
-  id uuid PRIMARY KEY not null,
+  id uuid PRIMARY KEY not null default gen_random_uuid(),
   title text not null,
   "number" int not null,
   manga_id uuid not null,
@@ -32,7 +39,7 @@ create table chapter (
 );
 
 create table chapter_image (
-  chapter_id uuid not null,
+  chapter_id uuid not null default gen_random_uuid(),
   image_id uuid not null,
   alignment int not null,
   PRIMARY KEY(chapter_id, image_id),
@@ -40,15 +47,8 @@ create table chapter_image (
   FOREIGN KEY(image_id) REFERENCES image(id)
 );
 
-create table account (
-  id uuid PRIMARY KEY,
-  name text not null unique,
-  base64_pwd_hash text not null,
-  base64_pwd_salt text not null
-);
-
 create table account_subscribed_manga (
-  id uuid PRIMARY KEY,
+  id uuid PRIMARY KEY not null default gen_random_uuid(),
   account_id uuid not null,
   manga_id uuid not null,
   FOREIGN KEY(manga_id) REFERENCES manga(id),
@@ -56,7 +56,7 @@ create table account_subscribed_manga (
 );
 
 create table account_viewed_chapter (
-  id uuid PRIMARY KEY,
+  id uuid PRIMARY KEY not null default gen_random_uuid(),
   account_id uuid not null,
   chapter_id uuid not null,
   viewed_at timestamp not null,
